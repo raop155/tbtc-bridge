fb71be0efff67a2a7d8a7231119a9929595c7739
Ricardo Olarte <raop155@hotmail.com>
2023-05-04 09:40:58 -0500

    feat: add threshold receibeTbtc

--- src/pages/Home/RedeemStep/index.tsx
+++ src/pages/Home/RedeemStep/index.tsx
@@ -1,8 +1,10 @@
+import { ThresholdL2WormholeGateway } from '@/ABI/ThresholdL2WormholeGateway';
 import Button from '@/components/Button';
-import { getTokenBridgeAddressForChain } from '@/consts';
+import { getTokenBridgeAddressForChain, THRESHOLD_GATEWAYS } from '@/consts';
 import { signedVaaHexAtom, targetWalletAtom } from '@/store/store';
 import parseError from '@/utils/parseError';
 import { hexToUint8Array, redeemOnEth, redeemOnEthNative } from '@certusone/wormhole-sdk';
+import { Contract } from 'ethers';
 import { useAtom } from 'jotai';
 
 const RedeemStep = () => {
@@ -62,9 +64,37 @@ const RedeemStep = () => {
     //   );
   };
 
+  const startRedeemGateway = async () => {
+    const { WH_CHAIN_ID: targetChainId, provider, signer } = targetWallet;
+    const targetAddress = THRESHOLD_GATEWAYS[targetChainId];
+
+    if (!signedVaaHex || !signer) return;
+    console.log('startRedeemGateway');
+    // const isNative = true;
+    try {
+      const signedVAA = signedVaaHex;
+      const L2WormholeGateway = new Contract(targetAddress, ThresholdL2WormholeGateway, provider);
+
+      console.log({
+        thresholdGateway: targetAddress,
+        L2WormholeGateway,
+        signer,
+        signedVAA,
+        hexToUint8Array_signedVAA: hexToUint8Array(signedVAA),
+      });
+
+      const receipt = await L2WormholeGateway.receiveTbtc(hexToUint8Array(signedVAA));
+
+      console.log({ receipt });
+      console.log('Transaction complete!');
+    } catch (e) {
+      console.log(parseError(e));
+    }
+  };
+
   return (
     <div className='w-full'>
-      <Button text='Redeem' style='purple' className='w-full' onClick={startRedeem} />
+      <Button text='Redeem' style='purple' className='w-full' onClick={startRedeemGateway} />
     </div>
   );
 };
